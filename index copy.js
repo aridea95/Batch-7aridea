895f8326dc23283345cb1751ef725de536155c1f
aridea95 <aridea95@gmail.com>
2020-10-28 21:21:45 +0700

    coding

---
+++ index copy.js
@@ -0,0 +1,28 @@
+function warnTheSheep(queue) {
+    // your code here
+    let counter = 0
+    let messages = [];
+    for (let i = queue.length-1; i >= 0; i--){
+        counter++
+        // let messages = [];
+        if(queue[queue.length-1] === "wolf"){
+            messages = "Pls go away and stop eating my sheep"
+        }else if(queue[i] === "wolf"){
+            messages = `Oi! Sheep number ${counter -1}! You are about to be eaten by a wolf!`
+        }
+    }  
+    return messages
+}
+
+
+// Test Function do not edit
+function Test(fun, result) {
+    console.log(fun === result)
+}
+
+
+// Test assertions
+Test(warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
+Test(warnTheSheep(["sheep", "sheep", "wolf"]), "Pls go away and stop eating my sheep");
+Test(warnTheSheep(["sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
+Test(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 6! You are about to be eaten by a wolf!");
